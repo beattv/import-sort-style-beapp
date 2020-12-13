bdad072a5df14422eb67b9ab7373c99a119a3162
Tomas Voslar <t.voslar@gmail.com>
2020-12-13 14:44:12 +0100

    Fix.

--- src/index.ts
+++ src/index.ts
@@ -50,14 +50,14 @@ export default function(styleApi: IStyleAPI): IStyleItem[] {
     {separator: true},
 
     {
-      match: isLocal,
+      match: isLocalGql,
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
     {separator: true},
 
     {
-      match: isLocalGql,
+      match: and(isLocal, not(isLocalGql)),
       sort: moduleName(naturally),
       sortNamedMembers: alias(unicode),
     },
